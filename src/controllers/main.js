773075e308024e2a69dc1bb47d542fb5884d34fe
Roman Tarnakin <roman_tarnakin@epam.com>
2023-01-23 18:55:43 +0300

    add username,password super basic validation

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -1,4 +1,15 @@
+const CustomAPIError = require('../errors/custom-error');
+
 const login = async (req, res) => {
+  const { username, password } = req.body;
+  // to validate these values we have several options:
+  // - Mongoose built-in validation
+  // - Joi (3rd party library)
+  // - we can write our own basic validation right here
+  if (!username || !password) {
+    throw new CustomAPIError('Please provide email and password', 400);
+  }
+
   res.send('fake Login/Register/SignUp');
 };
 
